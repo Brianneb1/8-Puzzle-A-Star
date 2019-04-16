4e3037b3f68dbf0fdf13cd4c70fcc9f00de786c2
Brianne Bender <s529231@nwmissouri.edu>
2019-04-15 19:57:22 -0500

    change list name

--- src/solutions.java
+++ src/solutions.java
@@ -5,17 +5,17 @@ public class solutions {
     public static ArrayList<Node> astar(Node start, Node goal){
         //heap fvalues based on heuristic
         ArrayList<Node> closed = new ArrayList<Node>();//closed list
-        minHeap fvalues = new minHeap(); //heap based on fvalues (open list)
+        minHeap open = new minHeap(); //heap based on fvalues (open list)
         ArrayList<Node> path = new ArrayList<Node>();
         int level = 0;
 
         //add starting node to open list
-        fvalues.add(start, level);
+        open.add(start, level);
 
-        while (fvalues.size() != 0)
+        while (open.size() != 0) //while elms left in open list
         {
             level++;
-            Node n = fvalues.remove(level); //get current node
+            Node n = open.remove(level); //get current node
 
             if (n.grid.equals(goal)) //if found goal state, recreate path
             {
@@ -32,26 +32,17 @@ public class solutions {
             for (int i = 0; i < children.size(); i++) //for each child of n
             {
                 Node child = children.get(i); //get child
-
                 if (closed.contains(child)) //if child in closed, skip
-                {
                     continue;
-                }
-
-                if(fvalues.contains(child)) //if child in open
+                if(open.contains(child)) //if child in open
                 {
                     if (child.gValue > n.gValue) //if child gVal > parent, skip
-                    {
                         continue;
-                    }
                 }
-
-                fvalues.add(child, level); //add child to open list
+                open.add(child, level); //add child to open list
             }
         }
-
         return path;
-
     }
 
 
