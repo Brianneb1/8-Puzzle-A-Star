036b5312e9a94794bd4315c60da90b43b9652692
Brianne Bender <s529231@nwmissouri.edu>
2019-04-25 14:42:32 -0500

    add node printing logic

--- src/main.java
+++ src/main.java
@@ -9,9 +9,9 @@ public class main {
 
         //call astar function on already complete puzzle, print completed path
         System.out.println("A* test on solved puzzle");
-        tester.astarTest(goal,goal);
+        tester.astarTest(goal,goal, true);
         System.out.println("Greedy BFS test on solved puzzle");
-        tester.greedyBFSTest(goal,goal);
+        tester.greedyBFSTest(goal,goal, true);
 
         //tester grid that's 2 numbers off/switched
         int[][] almostThere = {{1,2,3},{4,5,6},{7,0,8}};
@@ -19,9 +19,9 @@ public class main {
 
         //call astar function on almost complete puzzle, store completed path
         System.out.println("A* test on almost solved puzzle");
-        tester.astarTest(almost,goal);
+        tester.astarTest(almost,goal, true);
         System.out.println("Greedy BFS test on almost solved puzzle");
-        tester.greedyBFSTest(almost,goal);
+        tester.greedyBFSTest(almost,goal, true);
 
         System.out.println("Random puzzle:");
         //fill matrix randomly (for testing)
@@ -34,50 +34,50 @@ public class main {
         if(solvable) //call astar function on random puzzle
         {
             System.out.println("\nA* test on random puzzle");
-            tester.astarTest(start,goal);
+            tester.astarTest(start,goal, false);
             System.out.println("\nGreedy BFS test on random puzzle");
-            tester.greedyBFSTest(start,goal);
+            tester.greedyBFSTest(start,goal, false);
         }
 
         System.out.println("THE END");
 
 
         //eventually.... i hope
-//        double astarTime = 0;
-//        double GBFSTime = 0;
-//
-//        //test both on 100 random puzzles
-//        for (int i = 0; i < 100; i++)
-//        {
-//            int[][] puzzle1 = tester.generateRandom();
-//            double startTime = 0;
-//            double endTime = 0;
-//            double totalTime = 0;
-//
-//            start = new Node(puzzle1, goalState, 0);
-//            solvable = solutions.isSolvable(start); //check if solvable based on inversions
-//            if(solvable) //call astar function on random puzzle
-//            {
-//                startTime = System.nanoTime();
-//                tester.astarTest(start, goal);
-//                endTime = System.nanoTime();
-//                totalTime = endTime - startTime;
-//                astarTime += totalTime;
-//
-//                //call GBFS function on random puzzle
-//                startTime = System.nanoTime();
-//                tester.greedyBFSTest(start, goal);
-//                endTime = System.nanoTime();
-//                totalTime = endTime - startTime;
-//                GBFSTime += totalTime;
-//            }
-//            else{
-//                if (i > 0)
-//                    i -= 1;
-//            }
-//        }
-//
-//        System.out.println("The average time of A* algorithm to solve 8 Puzzle: " + astarTime/100);
-//        System.out.println("The average time of Greedy Best First algorithm to solve 8 Puzzle: " + GBFSTime/100);
+        double astarTime = 0;
+        double GBFSTime = 0;
+
+        //test both on 100 random puzzles
+        for (int i = 0; i < 100; i++)
+        {
+            int[][] puzzle1 = tester.generateRandom();
+            double startTime = 0;
+            double endTime = 0;
+            double totalTime = 0;
+
+            start = new Node(puzzle1, goalState, 0);
+            solvable = solutions.isSolvable(start); //check if solvable based on inversions
+            if(solvable) //call astar function on random puzzle
+            {
+                startTime = System.nanoTime();
+                tester.astarTest(start, goal, false);
+                endTime = System.nanoTime();
+                totalTime = endTime - startTime;
+                astarTime += totalTime;
+
+                //call GBFS function on random puzzle
+                startTime = System.nanoTime();
+                tester.greedyBFSTest(start, goal, false);
+                endTime = System.nanoTime();
+                totalTime = endTime - startTime;
+                GBFSTime += totalTime;
+            }
+            else{
+                if (i > 0)
+                    i -= 1;
+            }
+        }
+
+        System.out.println("The average time of A* algorithm to solve 8 Puzzle: " + astarTime/100);
+        System.out.println("The average time of Greedy Best First algorithm to solve 8 Puzzle: " + GBFSTime/100);
     }
 }
--- src/tester.java
+++ src/tester.java
@@ -2,25 +2,28 @@ import java.util.*;
 
 public class tester {
 
-    public static void astarTest(Node start, Node goal){
+    public static void astarTest(Node start, Node goal, boolean print){
 
         //call astar function on already complete puzzle, store completed path
         ArrayList<Node> tester = new solutions().astar(start, goal);
-        for (int i = tester.size()-1; i >= 0; i--)
+        if(print)
         {
-            System.out.println("Node "+i+":");
-            tester.get(i).printGrid();
+            for (int i = tester.size() - 1; i >= 0; i--) {
+                System.out.println("Node " + i + ":");
+                tester.get(i).printGrid();
+            }
         }
-
     }
 
-    public static void greedyBFSTest(Node start, Node goal){
+    public static void greedyBFSTest(Node start, Node goal, boolean print){
         //call astar function on already complete puzzle, store completed path
         ArrayList<Node> tester = new solutions().greedyBestFirst(start, goal);
-        for (int i = tester.size()-1; i >= 0; i--)
+        if(print)
         {
-            System.out.println("Node "+i+":");
-            tester.get(i).printGrid();
+            for (int i = tester.size() - 1; i >= 0; i--) {
+                System.out.println("Node " + i + ":");
+                tester.get(i).printGrid();
+            }
         }
     }
 
